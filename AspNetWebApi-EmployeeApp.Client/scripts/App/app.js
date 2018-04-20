f8173fffe75bc2a09c87faf976c980037751ccbf
Asif Rahman <asif0531@live.com>
2018-04-20 17:16:06 +0600

    aspnetwebapi client project added with jquery

---
+++ AspNetWebApi-EmployeeApp.Client/scripts/App/app.js
@@ -0,0 +1,33 @@
+﻿var baseUrl = "http://localhost:13968/api";
+
+$(document).ready(function () {
+    loadDepertment();
+
+    loadEmployee();
+});
+
+function loadDepertment() {
+    $.ajax({
+        url: baseUrl + "/Deparment",
+        type: "GET",
+        success: function (data) {
+            $.each(data, function (i, v) {
+                $("#DeptTable tbody").append("<tr> <td>" + v.Id + "</td> <td>" +v.Name + "</td> <td> </td></tr>");
+            })
+        }
+    })
+}
+
+
+
+function loadEmployee() {
+    $.ajax({
+        url: baseUrl + "/Employee",
+        type: "GET",
+        success: function (data) {
+            $.each(data, function (i, v) {
+                $("#EmpTable tbody").append("<tr> <td>" + v.Name + "</td> <td>" + v.Email + "</td> <td>" + v.Sallary + "</td> <td>" + v.Designation + "</td> </tr>");
+            })
+        }
+    })
+}
--- AspNetWebApi-EmployeeApp/App_Start/WebApiConfig.cs
+++ AspNetWebApi-EmployeeApp/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Cors;
 
 namespace AspNetWebApi_EmployeeApp
 {
@@ -10,6 +11,8 @@ namespace AspNetWebApi_EmployeeApp
         public static void Register(HttpConfiguration config)
         {
             // Web API configuration and services
+            var cors = new EnableCorsAttribute("http://localhost:8300", "*", "*");
+            config.EnableCors(cors);
 
             // Web API routes
             config.MapHttpAttributeRoutes();
